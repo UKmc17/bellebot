675f767803a36ee341c9ffa93dd23099c7dfb16b
UKmc17 <33585896+UKmc17@users.noreply.github.com>
2017-11-27 17:23:13 +0000

    {#23}

--- server.js
+++ server.js
@@ -1,50 +1,61 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs")
+var auth = require('./config.json');
+var logger = require('winston');
 
-client.on('ready', function (evt) {
 
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
 });
-client.on('message', function (user, userID, channelID, message, evt, client, msg, args)   {
-const commands = {
-	'play': (msg) => {
-		return msg.channel.sendMessage('You was tested for `HIV` and came back positve!);
-                client.sendMessage({
+logger.level = 'debug';
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt, client, msg, args)   {
+
+    if (message.substring(0, 1) == '-') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            case 'Test':
+                bot.sendMessage({
                     to: channelID,
                     message: 'You was tested for `HIV` and came back positve!'
-					       
-	client.on('message', function (user, userID, channelID, message, evt, client, msg, args)   {
-const commands = {
-	'play': (msg) => {
-		return msg.channel.sendMessage('You was tested for `HIV` and came back positve!);
-                client.sendMessage({
-                    to: channelID,
-                    message: 'You was tested for `HIV` and came back positve!'				       
                 });
             break;
 			case 'Me':
-                client.sendMessage({
+                bot.sendMessage({
                     to: channelID,
                     message: 'Meh, ur life nearly as boring as my code'
                 });
             break;
 			case 'Slap':
-                client.sendMessage({
+                bot.sendMessage({
                     to: channelID,
                     message: 'Bitch get out my way!'
                 });
             break;
 			case 'woof':
-                client.sendMessage({
+                bot.sendMessage({
                     to: channelID,
                     message: 'Woof!'
                 });
             break;
 			case 'Ily':
-                client.mentions.userID.first();
-                client.sendMessage({
+                bot.mentions.userID.first();
+                bot.sendMessage({
                   to: channelID,
-                  message: "I love you (userID)"
+                  message: "I love you ${message.member.username}"
               });
          }
      }
